707b9373c38b0ea57a1aa6cb2f3aec07176062b9
twf581 <mobileonsitehandyman@gmail.com>
2026-04-19 22:18:26 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Page() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Repair Estimator App</h1>
+      <p>If you see this, your app is working.</p>
+    </div>
+  );
+}
